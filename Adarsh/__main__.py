9c3d67c945ffa833e7d0ccaf2b78f5a054de361a
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-08-27 17:16:07 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -76,7 +76,7 @@ async def start_services():
     if Var.ON_HEROKU:
         print('                        app runnng on =>> {}'.format(Var.FQDN))
     print('---------------------------------------------------------------------------------------------------------')
-    print('Give a star to my repo https://github.com/adarsh-goel/filestreambot-pro  also follow me for new bots')
+    print('Give a star to my repo https://github.com/nikhilvishwa7/filestreambot-pro  also follow me for new bots')
     print('---------------------------------------------------------------------------------------------------------')
     await idle()
 
